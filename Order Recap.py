3148892615d4cb6e299263c846f74cf9e56195a1
scarlet-laurent <106478515+scarlet-laurent@users.noreply.github.com>
2025-03-11 18:16:40 +0700

    Update Order Recap.py

--- Order Recap.py
+++ Order Recap.py
@@ -2,24 +2,6 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 
-# Upload file Excel
-st.title("Dashboard Analisis Penjualan")
-uploaded_file = st.file_uploader("Unggah file Excel", type=["xlsx", "xls"])
-
-if uploaded_file is not None:
-    # Membaca data Excel
-    xls = pd.ExcelFile(uploaded_file)
-    
-    # Pastikan sheet 'orders' ada
-    if "Data Orders" in xls.sheet_names:
-        df = pd.read_excel(xls, sheet_name="Data Orders")
-        
-        # Menampilkan data
-        st.write("### Data yang diunggah (Sheet: Data Orders):")
-        st.dataframe(df)
-    else:
-        st.warning("Sheet 'Data Orders' tidak ditemukan dalam file Excel.")
-
 # Set judul dan ikon pada browser
 st.set_page_config(
     page_title='Data Penjualan PT Optima Technology Indonesia',
@@ -29,3 +11,20 @@ st.set_page_config(
 # Menampilkan logo di sidebar
 st.sidebar.image('logo.png', use_column_width=True)
 
+# Upload file Excel
+st.title("Dashboard Analisis Penjualan")
+uploaded_file = st.file_uploader("Unggah file Excel", type=["xlsx", "xls"])
+
+if uploaded_file is not None:
+    # Membaca data Excel
+    xls = pd.ExcelFile(uploaded_file)
+    
+    # Pastikan sheet 'Data Orders' ada
+    if "Data Orders" in xls.sheet_names:
+        df = pd.read_excel(xls, sheet_name="Data Orders")
+        
+        # Menampilkan data
+        st.write("### Data yang diunggah (Sheet: Data Orders):")
+        st.dataframe(df)
+    else:
+        st.warning("Sheet 'Data Orders' tidak ditemukan dalam file Excel.")
